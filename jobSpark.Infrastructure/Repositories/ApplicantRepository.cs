35e038a7ca9ec54b50d13dedc44744eb2ca5af82
DESKTOP-OD70FRL\mohamed kadry <muhammadkadry22@gmail.com>
2024-05-07 12:38:30 +0300

    GenericRepoWithUOW

---
+++ jobSpark.Infrastructure/Repositories/ApplicantRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using jobSpark.Domain.Entities;
+using jobSpark.Infrastructure.Abstractions;
+using jobSpark.Infrastructure.Context;
+using jobSpark.Infrastructure.InfrastructureBases;
+namespace jobSpark.Infrastructure.Repositories
+{
+    public class ApplicantRepository : GenericRepository<applicant>,IApplicantRepository
+    {
+        
+        public ApplicantRepository(ApplicationDbContext context) : base(context)
+        {
+            
+        }
+    }
+}
