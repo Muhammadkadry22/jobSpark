06f3cd6708016c79027251676d86dcec691a07b5
Ahmed Reda <95850640+AhmedReda103@users.noreply.github.com>
2024-05-14 22:38:01 +0300

    add applicantById EndPoint inProgress

---
+++ jobSpark.Service/implementations/ApplicantService.cs
@@ -0,0 +1,22 @@
+﻿using jobSpark.Domain.Entities;
+using jobSpark.Infrastructure.UnitOfWork;
+using jobSpark.Service.Abstracts;
+
+namespace jobSpark.Service.implementations
+{
+    public class ApplicantService : IApplicantService
+    {
+
+        private readonly IUnitOfWork unitOfWork;
+        public ApplicantService(IUnitOfWork unitOfWork) { this.unitOfWork = unitOfWork; }
+
+
+
+        public async Task<Applicant> GetApplicantByIdAsync(int id)
+        {
+            return await unitOfWork.applicants.GetByIdAsync(id);
+        }
+
+
+    }
+}
