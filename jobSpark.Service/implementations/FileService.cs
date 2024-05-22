7e7376a7578159128e77e3bfadb3c88885200911
Tarek <tareknasr1994@gmail.com>
2024-05-22 06:46:44 +0300

    Modify CV path

--- jobSpark.Service/implementations/FileService.cs
+++ jobSpark.Service/implementations/FileService.cs
@@ -1,26 +1,21 @@
 ﻿using jobSpark.Service.Abstracts;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 namespace jobSpark.Service.implementations
 {
     public class FileService : IFileService
     {
         private readonly IWebHostEnvironment _webHostEnvironment;
-        public FileService(IWebHostEnvironment webHostEnvironment) 
-        { 
+        public FileService(IWebHostEnvironment webHostEnvironment)
+        {
             _webHostEnvironment = webHostEnvironment;
         }
         public async Task<string> UploadFile(string location, IFormFile file)
         {
-            var path = _webHostEnvironment.WebRootPath+"/"+location;
-            var extention=Path.GetExtension(file.FileName);
-            var fileName=Guid.NewGuid().ToString().Replace("-",string.Empty)+extention;
-        if (file.Length>0)
+            var path = _webHostEnvironment.WebRootPath + "/" + location + "/";
+            var extention = Path.GetExtension(file.FileName);
+            var fileName = Guid.NewGuid().ToString().Replace("-", string.Empty) + extention;
+            if (file.Length > 0)
             {
                 try
                 {
@@ -32,14 +27,14 @@ namespace jobSpark.Service.implementations
                     {
                         await file.CopyToAsync(fileStream);
                         await fileStream.FlushAsync();  //remove for puffer
-                        return $"{path}/{fileName}";
+                        return $"{path + fileName}";
                     }
                 }
                 catch (Exception)
                 {
                     return "FailedToUploadFile";
                 }
-             
+
             }
             else
             {
